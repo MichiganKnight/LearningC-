98f0d5ef92f60cabdc415ab3c11dee9790eeca3a
Andrew Fleming <michiganknight2@gmail.com>
2026-06-16 15:31:39 -0500

    Added a Menu

--- main.cpp
+++ main.cpp
@@ -8,34 +8,70 @@ void printEmptySpace() {
     std::cout << std::endl;
 }
 
-int main() {
-    std::cout << "Hello World!" << std::endl;
-
-    simpleMath();
+void menu() {
+    std::cout << "=== Tutorial Menu ===" << std::endl;
 
     printEmptySpace();
+
+    std::cout << "1. Simple Math" << std::endl;
+    std::cout << "2. Simple Functions" << std::endl;
+    std::cout << "3. Return Five" << std::endl;
+    std::cout << "4. Get Value From User" << std::endl;
+    std::cout << "5. Exit" << std::endl;
+
+    printEmptySpace();
+
+    std::cout << "Enter a Number (1-5): ";
+
+    int choice;
+    std::cin >> choice;
+
+    switch (choice) {
+        case 1:
+            simpleMath();
+            break;
+
+        case 2:
+            doA();
+            break;
+
+        case 3:
+            std::cout << returnFive() << std::endl;
+            std::cout << returnFive() + 2 << std::endl;
+            break;
+
+        case 4: {
+            int x = getValueFromUser();
+            int y = getValueFromUser();
+
+            std::cout << x << " + " << y << " = " << (x + y) << std::endl;
+
+            break;
+        }
+
+        case 5:
+            std::cout << "Exiting..." << std::endl;
+            break;
+
+        default:
+            std::cout << "Invalid Choice" << std::endl;
+            break;
+    }
+
+}
+
+int main() {
+    std::cout << "Hello World!" << std::endl;
 
     std::cout << "Starting main()" << std::endl;
 
     printEmptySpace();
 
-    doA();
+    menu();
 
     printEmptySpace();
 
     std::cout << "Ending main()" << std::endl;
-
-    printEmptySpace();
-
-    std::cout << returnFive() << std::endl;
-    std::cout << returnFive() + 2 << std::endl;
-
-    printEmptySpace();
-
-    int x = getValueFromUser();
-    int y = getValueFromUser();
-
-    std::cout << x << " + " << y << " = " << (x + y) << std::endl;
 
     return 0;
 }
